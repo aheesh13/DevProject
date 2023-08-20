97f651a2370ba0a5f2e0626ac7c4f7d32331f70c
aheesh13 <aheesh13@gmail.com>
2023-08-20 18:23:28 +0530

    new setup of Movie Application

--- redbus/src/main/java/com/movie/bookticket/repository/LocationRepository.java
+++ redbus/src/main/java/com/movie/bookticket/repository/LocationRepository.java
@@ -1,9 +1,11 @@
 package com.movie.bookticket.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.movie.bookticket.model.Location;
 
+@RepositoryRestResource
 public interface LocationRepository extends JpaRepository<Location, Long>{
 
 }
--- redbus/src/main/java/com/movie/bookticket/repository/TheaterRepository.java
+++ redbus/src/main/java/com/movie/bookticket/repository/TheaterRepository.java
@@ -1,9 +1,11 @@
 package com.movie.bookticket.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.movie.bookticket.model.Theater;
 
+@RepositoryRestResource
 public interface TheaterRepository extends JpaRepository<Theater, Long> {
 	
 }
